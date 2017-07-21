50dcca7a1d1d9b228f633674a548e5053431de75
Abdullah Sheikh <jojopeter3@gmail.com>
2017-07-21 15:04:47 +0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { HomeComponent } from './orders/home/home.component';
+import { CreateOrderComponent } from './orders/create-order/create-order.component';
+import { EditOrderComponent } from './orders/edit-order/edit-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavmenuComponent,
+    HomeComponent,
+    CreateOrderComponent,
+    EditOrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: '', redirectTo: 'orders', pathMatch: 'full' },
+        { path: 'orders', component: HomeComponent },
+        { path: 'orders/create', component: CreateOrderComponent },
+        { path: 'orders/edit/:id', component: EditOrderComponent },
+        { path: '**', redirectTo: 'orders' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
